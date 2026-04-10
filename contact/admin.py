9e9b8f4a71944be132cfaa092968324853018163
Everton Espedito <evertondev505@gmail.com>
2026-04-10 14:17:45 -0300

    Update

--- contact/admin.py
+++ contact/admin.py
@@ -9,9 +9,10 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ( 'id','first_name', 'last_name', 'phone', 'email', 'created_date')
+    list_display = ( 'id','first_name', 'last_name', 'phone', 'email', 'created_date','show')
     ordering = ('-id',)
     search_fields = ('id', 'first_name', 'last_name', 'email')
     list_filter = ('created_date',)
+    list_editable = ('first_name', 'last_name', 'phone', 'email', 'show') 
     list_per_page = 20
     list_max_show_all = 100
