bbf94f17b41045a65057c11b328a5919a56d7ccb
Everton Espedito <evertondev505@gmail.com>
2026-04-14 13:18:04 -0300

    Template Sobre
    Criação do template 'Sobre', com detalhes básicos do projeto.

--- contact/urls.py
+++ contact/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from contact import views
+from . import views
 
 urlpatterns = [
     path('<int:contact_id>/', views.contact, name='contact'),
     path('', views.index, name='index'),
     path('search/', views.search, name='search'),
+    path('sobre/', views.sobre, name='about'),
 ]
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -15,6 +15,10 @@ def index(request):
         context
     )
 
+def sobre(request):
+    context = {'site_title': 'Sobre'}
+    return render(request, 'sobre/about.html', context)
+
 def contact(request, contact_id):
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
 
